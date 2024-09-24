018029a9864ee06bc15f706e5ce6ed7ae8036ac2
Stalker05 <ruslangallamov22@gmail.com>
2024-09-24 21:43:27 +0300

    Add files via upload

---
+++ lesson0 by Gallyamov.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"1st program"
+print((9 ** 0.5) * 5)
+"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1) # True
+"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2))) # false
+"4th program"
+(float('123.456')* 10)
+x=(float('123.456')* 10)
+print(x % 10)
+y=(x % 10)
+print(y // 1)
+z=(y // 1)
+print((int(z)))
+w=(int(z))
+print(w)
